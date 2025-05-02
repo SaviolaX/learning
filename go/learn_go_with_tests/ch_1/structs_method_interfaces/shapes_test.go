90b0c392c4e55461b1252f1db49998d46e72ea91
SaviolaX <withcraft1717@gmail.com>
2025-05-02 15:53:21 +0300

    update go

--- go/learn_go_with_tests/ch_1/structs_method_interfaces/shapes_test.go
+++ go/learn_go_with_tests/ch_1/structs_method_interfaces/shapes_test.go
@@ -2,35 +2,35 @@ package main
 
 import "testing"
 
-func TestPerimeter(t *testing.T)  {
-    rectangle := Rectangle{10.0, 10.0}
-    got := Perimeter(rectangle)
-    want := 40.0
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
 
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    }
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
 }
 
 func TestArea(t *testing.T) {
-    areaTests := []struct {
-        name string
-        shape Shape
-        hasArea float64
-    }{
-        {shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-        {shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-        {shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
-    }
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
 
-    for _, tt := range areaTests {
-        t.Run(tt.name, func(t *testing.T) {
-            got := tt.shape.Area()
-            if got != tt.hasArea{
-                t.Errorf("%#v got %g want %g",tt.shape, got, tt.hasArea)
-            }
-        })
-    }
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
 
 // func TestArea(t *testing.T) {
@@ -40,16 +40,16 @@ func TestArea(t *testing.T) {
 //         if got != want {
 //             t.Errorf("got %g want %g", got, want)
 //         }
-//         
+//
 //     }
 //     t.Run("rectangle", func(t *testing.T) {
 //         rectangle := Rectangle{12.0, 6.0}
 //         checkArea(t, rectangle, 72.0)
-// 
+//
 //     })
 //    t.Run("circle", func(t *testing.T) {
 //         circle := Circle{10}
 //         checkArea(t, circle, 314.1592653589793)
 //     })
-// 
+//
 // }
