8f78d698c16fba34319ad9fb0c539926d4f92cc5
SaviolaX <withcraft1717@gmail.com>
2025-05-13 16:26:03 +0300

    update go

---
+++ go/learn_go_with_tests/templating/renderer.go
@@ -0,0 +1,56 @@
+package blogrenderer
+
+import (
+	"io"
+    "html/template"
+    "embed"
+    "github.com/gomarkdown/markdown"
+    "github.com/gomarkdown/markdown/parser"
+)
+
+var (
+    //go:embed "templates/*"
+    postTemplates embed.FS
+)
+
+type PostRenderer struct {
+    templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+    templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+    if err != nil {
+        return nil, err
+    }
+
+    return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+    
+    if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+     
+    if err := r.templ.ExecuteTemplate(w, "index.gohtml", posts); err != nil {
+        return err
+    }
+
+    return nil
+ }
+
+type postViewModel struct {
+    Post
+    HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+    vm := postViewModel{Post: p}
+    vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+    return vm
+}
