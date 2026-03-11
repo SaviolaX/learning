ecf2eecdb79fd0ac3478bf4b51143ae389da6a17
SaviolaX <withcraft1717@gmail.com>
2026-03-11 15:03:00 +0200

    refactor: rename struct to URLPair

--- go/mini_apps/url_shortener/pkg/storage/storage.go
+++ go/mini_apps/url_shortener/pkg/storage/storage.go
@@ -1,10 +1,30 @@
 package storage
 
-type Storage struct {
-	LongUrl  string
-	ShortUrl string
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type URLPair struct {
+	LongUrl  string `json:"longUrl"`
+	ShortUrl string `json:"shortUrl"`
 }
 
-func (s Storage) Write() {}
+func (u URLPair) Store(filename string) error {
 
-func (s Storage) Read() {}
+	// convert storage into bytes
+	data, err := json.MarshalIndent(u, "", " ")
+	if err != nil {
+		return fmt.Errorf("marshal storage: %w", err)
+	}
+
+	// write bytes into a file
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("write file: %w", err)
+	}
+
+	return nil
+
+}
