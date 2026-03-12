cfd92bf6a347fc4fe59bbeceb6cf97426bce8b8f
SaviolaX <withcraft1717@gmail.com>
2026-03-12 12:53:16 +0200

    feat: create Repository struct, add Load

--- go/mini_apps/url_shortener/pkg/storage/storage.go
+++ go/mini_apps/url_shortener/pkg/storage/storage.go
@@ -11,20 +11,39 @@ type URLPair struct {
 	ShortUrl string `json:"shortUrl"`
 }
 
-func (u URLPair) Store(filename string) error {
+type Repository struct {
+	DbPath string
+}
+
+func (r *Repository) Store(urls []URLPair) error {
 
 	// convert storage into bytes
-	data, err := json.MarshalIndent(u, "", " ")
+	data, err := json.MarshalIndent(urls, "", " ")
 	if err != nil {
 		return fmt.Errorf("marshal storage: %w", err)
 	}
 
 	// write bytes into a file
-	err = os.WriteFile(filename, data, 0644)
+	err = os.WriteFile(r.DbPath, data, 0644)
 	if err != nil {
 		return fmt.Errorf("write file: %w", err)
 	}
 
 	return nil
+}
 
+func (r *Repository) Load() ([]URLPair, error) {
+
+	var result []URLPair
+
+	data, err := os.ReadFile(r.DbPath)
+	if err != nil {
+		return result, fmt.Errorf("read file: %w", err)
+	}
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return result, fmt.Errorf("unmarshal storage: %w", err)
+	}
+
+	return result, nil
 }
