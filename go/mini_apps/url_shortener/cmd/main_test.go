3c9feeb44a3d11c6f9caf6ddaa726311c55f00a8
SaviolaX <withcraft1717@gmail.com>
2026-03-13 19:24:23 +0200

    test: add unit tests for home page and BuildMap function

---
+++ go/mini_apps/url_shortener/cmd/main_test.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+	"urlShortener/pkg/storage"
+)
+
+func TestHomePage_MethodNotAllowed(t *testing.T) {
+	repo := storage.Repository{DbPath: ""}
+
+	s := &Server{
+		repo:      repo,
+		indexPath: "../templates/index.html",
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", s.homePage)
+
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	rec := httptest.NewRecorder()
+
+	router.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusMethodNotAllowed {
+		t.Fatalf("expected status %d got %d", http.StatusMethodNotAllowed, rec.Code)
+	}
+}
+
+func TestHomePage_GET(t *testing.T) {
+	repo := storage.Repository{DbPath: ""}
+
+	s := &Server{
+		repo:      repo,
+		indexPath: "../templates/index.html",
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", s.homePage)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+
+	router.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf("expected status %d got %d", http.StatusOK, rec.Code)
+	}
+
+	if !strings.Contains(rec.Body.String(), "<html") {
+		t.Fatalf("expected html response")
+	}
+}
+
+func TestBuildMap_MapLoadedData(t *testing.T) {
+	pairs := []storage.URLPair{
+		{
+			LongUrl:  "asdf",
+			ShortUrl: "qwe",
+		},
+
+		{
+			LongUrl:  "asdf",
+			ShortUrl: "qwe",
+		},
+	}
+
+	got := BuildMap(pairs)
+	want := map[string]string{
+		pairs[0].ShortUrl: pairs[0].LongUrl,
+		pairs[1].ShortUrl: pairs[1].LongUrl,
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %+v, want %+v", got, want)
+	}
+}
+
+func TestBuildMap_MapEmptyList(t *testing.T) {
+	pairs := []storage.URLPair{}
+
+	got := BuildMap(pairs)
+	want := map[string]string{}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %+v, want %+v", got, want)
+	}
+}
