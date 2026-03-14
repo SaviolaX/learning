1a7b8a2504a88a4e5e1ade0397d8af2755a8c423
SaviolaX <withcraft1717@gmail.com>
2026-03-14 15:47:34 +0200

    feat: implement URL validation and normalization in NewUrl function

---
+++ go/mini_apps/url_shortener_v1/internal/url/url.go
@@ -0,0 +1,43 @@
+package url
+
+import (
+	"errors"
+	"fmt"
+	neturl "net/url"
+	"strings"
+)
+
+type Url struct {
+	Value string
+}
+
+func NewUrl(originalUrl string) (Url, error) {
+
+	if len(originalUrl) == 0 {
+		return Url{}, errors.New("url is empty")
+	}
+
+	originalUrl = strings.TrimSpace(originalUrl)
+
+	if !strings.HasPrefix(originalUrl, "http://") && !strings.HasPrefix(originalUrl, "https://") {
+		originalUrl = "https://" + originalUrl
+	}
+
+	parsed, err := neturl.ParseRequestURI(originalUrl)
+	if err != nil {
+		return Url{}, fmt.Errorf("invalid originalUrl: %w", err)
+	}
+
+	if parsed.Host == "" {
+		return Url{}, errors.New("missing host")
+	}
+
+	if !strings.Contains(parsed.Host, ".") {
+		return Url{}, errors.New("invalid host")
+	}
+
+	normalized := parsed.String()
+
+	return Url{Value: normalized}, nil
+
+}
