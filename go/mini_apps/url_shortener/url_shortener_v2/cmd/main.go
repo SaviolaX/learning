967afce10ff9524a8d832912faf6a6bc46716be4
SaviolaX <withcraft1717@gmail.com>
2026-03-20 13:16:53 +0200

    refactor: switch from JSON to SQLite repository in main function

--- go/mini_apps/url_shortener/url_shortener_v2/cmd/main.go
+++ go/mini_apps/url_shortener/url_shortener_v2/cmd/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"log"
 	application "urlShortenerV2/internal/application/url"
-	"urlShortenerV2/internal/infrastructure/databases/json"
+	sqlite "urlShortenerV2/internal/infrastructure/databases/sqlite"
 	h "urlShortenerV2/internal/infrastructure/hasher"
 	transp "urlShortenerV2/internal/transport/http"
 )
 
-const dbPath = "UrlShortenerDB.json"
+const jsonDbPath = "UrlShortenerDB.json"
+const sqliteDbPath = "urls.db"
 const indexPath = "templates/index.html"
 const maxCodeLen = 10
 
@@ -15,7 +17,10 @@ var port = ":8080"
 var host = "localhost"
 
 func main() {
-	repo := json.NewRepository(dbPath)
+	repo, err := sqlite.NewRepository(sqliteDbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	service := application.NewService(repo, h.Sha256, maxCodeLen)
 
